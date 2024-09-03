680c65cccee6be22c507d988efc02f261861b67d
Pavel Kalaš <ten.neznamey@seznam.cz>
2024-09-03 12:31:00 +0200

    Added simple logics, if player join, they will be added to list, when quit, they will be removed also from the list

--- src/cz/pavelkalas/listeners/ConnectionListener.java
+++ src/cz/pavelkalas/listeners/ConnectionListener.java
@@ -1,12 +1,28 @@
 package cz.pavelkalas.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionListener extends PlayerListener implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		
+		if (!unloggedPlayers.contains(player)) {
+			unloggedPlayers.add(player);
+		}
+	}
+
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		
+		if (unloggedPlayers.contains(player)) {
+			unloggedPlayers.remove(player);
+		}
 	}
 }
