68287071976ab4760760d36fd54060473429170c
Pavel Kalaš <ten.neznamey@seznam.cz>
2024-09-03 18:46:32 +0200

    Added functions with customized message with prefix

--- src/cz/pavelkalas/utils/Messager.java
+++ src/cz/pavelkalas/utils/Messager.java
@@ -1,4 +1,21 @@
 package cz.pavelkalas.utils;
 
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+/**
+ * Class for customizing chat messages, adding prefixes to messages etc.
+ */
 public class Messager {
+	
+	/**
+	 * Adds a custom prefix to message.
+	 * 
+	 * @param player - Player instance 
+	 * @param message - Message content
+	 */
+	public static void sendMessage(Player player, String message) {
+		player.sendMessage(ChatColor.RED + "Auth" + ChatColor.GOLD+ "Me" + ChatColor.YELLOW +"Master" + ChatColor.WHITE + " >> " + message);
+	}
 }
