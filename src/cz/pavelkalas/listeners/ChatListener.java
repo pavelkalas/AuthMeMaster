2478ebdb1d6e905deb109df0e3b126dc229a4c12
Pavel Kalaš <ten.neznamey@seznam.cz>
2024-09-03 12:24:24 +0200

    Added class for chat listening

--- src/cz/pavelkalas/listeners/ChatListener.java
+++ src/cz/pavelkalas/listeners/ChatListener.java
@@ -1,6 +1,12 @@
 package cz.pavelkalas.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
+	
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent event) {
+	}
 }
