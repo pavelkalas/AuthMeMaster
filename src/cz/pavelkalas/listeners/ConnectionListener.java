9124556e58fc2de48a2930edfc2c1252bafdd29b
Pavel Kalaš <ten.neznamey@seznam.cz>
2024-09-03 12:28:37 +0200

    Added arraylist for players and added connection listener class

---
+++ src/cz/pavelkalas/listeners/ConnectionListener.java
@@ -0,0 +1,12 @@
+package cz.pavelkalas.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class ConnectionListener extends PlayerListener implements Listener {
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+	}
+}
--- src/cz/pavelkalas/listeners/PlayerListener.java
+++ src/cz/pavelkalas/listeners/PlayerListener.java
@@ -1,10 +1,16 @@
 package cz.pavelkalas.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerListener implements Listener {
+	
+	public List<Player> unloggedPlayers = new ArrayList<Player>();
 
 	@EventHandler
 	public void onMove(PlayerMoveEvent event) {
